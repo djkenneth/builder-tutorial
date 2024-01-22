234b5d3bce504a980e4e6cc8499faa64003c34ff
djkenneth <dj.kenneth.pineda@gmail.com>
2024-01-22 20:51:49 +0800

    update builder function

--- src/utils/builder/builderSDKFunctions.ts
+++ src/utils/builder/builderSDKFunctions.ts
@@ -1,5 +1,7 @@
 import { builder } from "@builder.io/sdk";
 
+const BUILDER_API_KEY = process.env.NEXT_PUBLIC_BUILDER_API_KEY || '';
+
 export const getBuilderDataModel = async (model: string) => {
     const data = await builder.get(model, {
         fields: 'data',
@@ -14,9 +16,16 @@ export const getAllBuilderDataModel = async (model: string) => {
         fields: 'data',
         options: { noTargeting: true },
         query: {
-            'data.title': { $regex: 'she', $options: 'i' }
+            'data.$or': [
+                { title: { $regex: 'mother', $options: 'i' } },
+                { description: { $regex: 'expert', $options: 'i' } },
+                { tags: { $in: ['crime'] } }
+            ]
         }
     })
+
+    // const data = await fetch(`https://cdn.builder.io/api/v3/content/${model}?apiKey=${BUILDER_API_KEY}&query.data.$or={title={ $regex: 'mother', $options: 'i' },{ description: { $regex: 'expert', $options: 'i' } }}`)
+    // const { results } = await data.json();
 
     return data;
 }
